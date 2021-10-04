97d43ec1291a2d9af7ee9f12be7f5ff9d24e1645
vhernandez.99@hotmail.com <Victorhdz1234>
2021-10-03 22:46:27 -0500

    1

--- src/components/Navbar/Navbar.elements.js
+++ src/components/Navbar/Navbar.elements.js
@@ -32,7 +32,7 @@ export const NavLogo = styled(Link)`
   text-decoration: none;
   font-size: 2rem;
   display: flex;
-  align-items: auto;
+  align-items: center;
   margin-top:5px;
   margin-bottom:5px;
 `;
